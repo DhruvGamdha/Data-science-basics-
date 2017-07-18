a0b713ba0b3265151df3b8a96877f2d0c730f5bd
Dhruv Gamdha <dhruvgamdha@gmail.com>
2017-07-18 10:02:20 +0530

    Basics of Data science

---
+++ 1- Read Data.R
@@ -0,0 +1,10 @@
+###################################
+#  ### DESCRIBING DATA ###
+############
+
+#Read the data using read.csv
+store.df <- read.csv(paste("StoreData.csv", sep=""))
+
+#View the data frame in R
+View(store.df)
+############
